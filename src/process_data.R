7d109c1633a5eb0f892118d8280e7b120b3da419
Jane Doe <jane@example.com>
2020-03-08 22:57:55 -0700

    added milestone 1 coding

--- src/process_data.R
+++ src/process_data.R
@@ -24,6 +24,22 @@ opt <- docopt(doc)
 
 # wrangle/clean/process your raw data and save a new version of data for later analysis
 
+# finding out how many NAs
+sum(is.na(suiciderates))/27820*12
+
+sum(is.na(suiciderates$HDI.for.year))/27820
+
+# getting rid of NAs, resulting in new dataset
+suicideratesnew <- suiciderates %>% 
+  select(-HDI.for.year)
+
+DT::datatable(suicideratesnew)
+
+# confirmation of NAs removed 
+sum(is.na(suicideratesnew))/27820*11
+
+#######
+
 main <-function(url){
   suicides <- read.csv('data/suicides.csv', row.names=1)
   write.csv(suicides, here ("data", "suiciderates.csv"))
